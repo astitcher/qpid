f35a6eb2c55cf91e7ef1111ec36c915e554c568e
Andrew Stitcher <astitcher@apache.org>
2013-04-11 18:10:50 -0400

    QPID-????: Change tcp nodelay to be the default - Attention to keep options backwards compatible

--- qpid/cpp/src/qpid/broker/Broker.h
+++ qpid/cpp/src/qpid/broker/Broker.h
@@ -106,6 +106,7 @@ class Broker : public sys::Runnable, public Plugin::Target,
         size_t replayHardLimit;
         uint queueLimit;
         bool tcpNoDelay;
+        bool dummy; // Hack to allow option with and without arguments
         bool requireEncrypted;
         std::string knownHosts;
         std::string saslConfigPath;
