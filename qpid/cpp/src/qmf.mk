aadf413ac8d38d31ed5c56add17317a0a74a137f
Andrew Stitcher <astitcher@apache.org>
2013-06-20 01:56:08 -0400

    QPID-4940: Remove obsolete qmf code - Remove qmf1 engine code - Remove qmf1 language bindings - Remove qmf1 agent code (it depends on engine) - Fix up cmake and autotools to build

--- qpid/cpp/src/qmf.mk
+++ qpid/cpp/src/qmf.mk
@@ -21,17 +21,8 @@
 # qmf library makefile fragment, to be included in Makefile.am
 # 
 lib_LTLIBRARIES +=	\
-  libqmf.la		\
-  libqmfengine.la	\
   libqmf2.la
 
-#
-# Public headers for the QMF API
-#
-QMF_API =					\
-  ../include/qpid/agent/ManagementAgent.h	\
-  ../include/qpid/agent/QmfAgentImportExport.h	\
-  ../include/qmf/BrokerImportExport.h
 
 #
 # Public headers for the QMF2 API
@@ -57,34 +48,9 @@ QMF2_API =				\
   ../include/qmf/Subscription.h
 
 
-#
-# Public headers for the QMF Engine API
-#
-QMF_ENGINE_API =				\
-  ../include/qmf/engine/Agent.h			\
-  ../include/qmf/engine/ConnectionSettings.h	\
-  ../include/qmf/engine/Console.h		\
-  ../include/qmf/engine/Event.h			\
-  ../include/qmf/engine/Message.h		\
-  ../include/qmf/engine/Object.h		\
-  ../include/qmf/engine/ObjectId.h		\
-  ../include/qmf/engine/QmfEngineImportExport.h	\
-  ../include/qmf/engine/Query.h			\
-  ../include/qmf/engine/ResilientConnection.h	\
-  ../include/qmf/engine/Schema.h		\
-  ../include/qmf/engine/Typecode.h		\
-  ../include/qmf/engine/Value.h
-
 # Public header files
 nobase_include_HEADERS +=	\
-  $(QMF_API)			\
-  $(QMF_ENGINE_API)		\
   $(QMF2_API)
-
-libqmf_la_SOURCES =			\
-  $(QMF_API)				\
-  qpid/agent/ManagementAgentImpl.cpp	\
-  qpid/agent/ManagementAgentImpl.h
 
 libqmf2_la_SOURCES = 		\
   $(QMF2_API)			\
@@ -133,45 +99,10 @@ libqmf2_la_SOURCES = 		\
   qmf/Subscription.cpp		\
   qmf/SubscriptionImpl.h
 
-libqmfengine_la_SOURCES =			\
-  $(QMF_ENGINE_API)				\
-  qmf/engine/Agent.cpp				\
-  qmf/engine/BrokerProxyImpl.cpp		\
-  qmf/engine/BrokerProxyImpl.h			\
-  qmf/engine/ConnectionSettingsImpl.cpp		\
-  qmf/engine/ConnectionSettingsImpl.h		\
-  qmf/engine/ConsoleImpl.cpp			\
-  qmf/engine/ConsoleImpl.h			\
-  qmf/engine/EventImpl.cpp			\
-  qmf/engine/EventImpl.h			\
-  qmf/engine/MessageImpl.cpp			\
-  qmf/engine/MessageImpl.h			\
-  qmf/engine/ObjectIdImpl.cpp			\
-  qmf/engine/ObjectIdImpl.h			\
-  qmf/engine/ObjectImpl.cpp			\
-  qmf/engine/ObjectImpl.h			\
-  qmf/engine/Protocol.cpp			\
-  qmf/engine/Protocol.h				\
-  qmf/engine/QueryImpl.cpp			\
-  qmf/engine/QueryImpl.h			\
-  qmf/engine/ResilientConnection.cpp		\
-  qmf/engine/SequenceManager.cpp		\
-  qmf/engine/SequenceManager.h			\
-  qmf/engine/SchemaImpl.cpp			\
-  qmf/engine/SchemaImpl.h			\
-  qmf/engine/ValueImpl.cpp			\
-  qmf/engine/ValueImpl.h
-
-libqmf_la_LIBADD = libqmfengine.la
 libqmf2_la_LIBADD = libqpidmessaging.la libqpidtypes.la
-libqmfengine_la_LIBADD = libqpidclient.la
 
-QMF_VERSION_INFO = 1:0:0
 QMF2_VERSION_INFO = 1:0:0
-QMFENGINE_VERSION_INFO  = 1:1:0
 
-libqmf_la_LDFLAGS = -version-info $(QMF_VERSION_INFO)
 libqmf2_la_LDFLAGS = -version-info $(QMF2_VERSION_INFO)
-libqmfengine_la_LDFLAGS = -version-info $(QMFENGINE_VERSION_INFO)
 
 pkgconfig_DATA += qmf2.pc
